5b7de69aaf367505e433fbed9fad83ee8ba53eae
GijonJovellanos2025 <olgafformacion@gmail.com>
2025-01-27 13:18:58 +0100

    ejercicio1

---
+++ ejercicio1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+    /* code */
+   int n1, n2, res ;
+   printf ("Ingrese el primer numero");
+   scanf ("%d",&n1);
+   printf("Ingrese el segundo numero");
+   scanf("%d",&n2);
+   res=n1+n2;
+   printf ("El resultado de la suma es %d",res);
+
+    return 0;
+}
