4f110abe462c7e0face444462d835c5289320bac
GijonJovellanos2025 <olgafformacion@gmail.com>
2025-01-28 09:05:04 +0100

    mu7ltipicacion

---
+++ Ejercicio2.c
@@ -0,0 +1,20 @@
+
+    #include<stdio.h>
+
+
+
+int main(void)
+{
+    /* code */
+    
+    int num1=0, num2=0, multiplicacion=0;
+    printf("Ingresa el primer numero:");
+    scanf("%d",&num1);
+    printf("Ingresa el segundo numero:");
+    scanf("%d",&num2);
+    printf("La multiplicacion de ambos numeros es %d\n",num1*num2);
+
+    return(0);
+    
+}
+
